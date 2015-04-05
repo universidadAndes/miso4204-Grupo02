d5dde90b953c2b82f347f55f54a613fe9ff8200c
coyot3 <ghnm67@gmail.com>
2015-04-05 13:13:03 -0500

    HTML del proyecto
    HTML del proyecto

---
+++ Proyecto_html/Proyecto_html/viejos/UserLib.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    //Wait for Pinegrow to wake-up
+    $("body").one("pinegrow-ready", function(e, pinegrow) {
+
+        //Create new Pinegrow framework object
+        var f = new PgFramework("UserLib", "UserLib");
+
+        //This will prevent activating multiple versions of this framework being loaded
+        f.type = "UserLib";
+        f.allow_single_type = true;
+        f.user_lib = true
+
+        var comp_comp1 = new PgComponentType('comp1', 'Comp 1 / h2');
+        comp_comp1.code = '<h2 class="featurette-heading crsa-selected">First featurette heading. <span class="text-muted">It\'ll blow your mind.</span></h2>';
+        comp_comp1.parent_selector = null;
+        f.addComponentType(comp_comp1);
+        
+        //Tell Pinegrow about the framework
+        pinegrow.addFramework(f);
+            
+        var section = new PgFrameworkLibSection("UserLib_lib", "Components");
+        //Pass components in array
+        section.setComponentTypes([comp_comp1]);
+
+        f.addLibSection(section);
+   });
+});
